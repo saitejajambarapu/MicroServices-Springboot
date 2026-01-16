e75d9cb4c3e90fea89152e6507524ccdd76c806a
saitejajambarapu <saitejajambrapu27@gmail.com>
2026-01-16 14:25:39 +0530

    dockerized the services and deployed it in gcp using kubernetes GKE. added K8s yml files for deplyment and service for the dockerized services containers

--- inventory/src/main/java/com/inventory/project/inventory/clients/OrdersFeignClient.java
+++ inventory/src/main/java/com/inventory/project/inventory/clients/OrdersFeignClient.java
@@ -3,7 +3,7 @@ package com.inventory.project.inventory.clients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name="orders", path = "/orders")
+@FeignClient(name="orders", path = "/orders", url="${ORDER_SERVICE:}")
 public interface OrdersFeignClient {
 
     @GetMapping("/core/hello")
--- orders/src/main/java/com/order/project/demo/clients/InventoryFeignClient.java
+++ orders/src/main/java/com/order/project/demo/clients/InventoryFeignClient.java
@@ -3,7 +3,7 @@ package com.order.project.demo.clients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "inventory", path = "/inventory")
+@FeignClient(name = "inventory", path = "/inventory", url = "${INVENTORY_SERVICE:}")
 public interface InventoryFeignClient {
     @GetMapping("/core/hello")
     String hello();
